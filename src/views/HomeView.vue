070d04ace2a1840c0bcbe5ed5cda61c43e9aa949
jarek.wabich <jaroslaw.wabich@wfirma.pl>
2022-06-21 22:12:01 +0200

    initial commit
    get questions from api
    show and increment questions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Wins Quiz</h1>
+    <button @click="startQuiz">START</button>
+       <h3 v-if="quizStarted">{{ questions[questionNo] }}</h3>
+       <h3 v-else>Rozpocznij quiz</h3>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {ref, watchEffect} from "vue";
+
+const API_URL = 'http://127.0.0.1:8000/api/question/';
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const questions = ref([]),
+        questionNo = ref(0),
+        quizStarted = ref(false);
+
+    watchEffect(async () => {
+      questions.value = await (await fetch(`${API_URL}`)).json();
+    })
+
+    const startQuiz = () => {
+      const questionsCount = questions.value.length;
+
+      if (!quizStarted.value) {
+        quizStarted.value = true
+      }
+
+      if (questionNo.value < questionsCount - 1) {
+        questionNo.value++;
+      } else {
+        console.info('last question');
+      }
+    }
+
+    return { questions, questionNo, quizStarted, startQuiz }
   }
 }
 </script>

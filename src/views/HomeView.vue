bf021d9b2d30624695bb93cd028dea81325eed51
jarek.wabich <jaroslaw.wabich@wfirma.pl>
2022-06-24 22:34:18 +0200

    next question only if answer store user answers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,10 @@
             <h3 class="pb-3">{{ questions[questionNo]['question'] }}</h3>
             <div class="row mx-auto justify-content-md-center pb-5 gx-5">
                 <div v-for="answerOption in answersArray" class="col-md-5 p-4">
-                    <button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo][`answer_${answerOption}`] }}</button>
+                    <button @click="setAnswer(answerOption)" class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo][`answer_${answerOption}`] }}</button>
                 </div>
             </div>
-            <button v-if="quizStarted" @click="startQuiz" class="btn btn-outline-dark mb-5">DALEJ</button>
+            <button v-if="quizStarted && userAnswer" @click="checkAnswer(); startQuiz()" class="btn btn-outline-dark mb-5">DALEJ</button>
         </div>
     </div>
 
@@ -28,7 +28,9 @@ export default {
         const questions = ref([]),
             questionNo = ref(0),
             quizStarted = ref(false),
-            answersArray = ref(['a', 'b', 'c', 'd']);
+            answersArray = ref(['a', 'b', 'c', 'd']),
+            userAnswer = ref(null),
+            userAnswers = [];
 
         watchEffect(async () => {
             questions.value = await (await fetch(`${API_URL}`)).json();
@@ -41,14 +43,35 @@ export default {
                 quizStarted.value = true
             }
 
-            if (questionNo.value < questionsCount - 1) {
+            if (questionNo.value < questionsCount - 1 && userAnswers.length == questionNo.value) {
                 questionNo.value++;
+                userAnswer.value = null
+            } else if (userAnswers.length < questionNo.value){
+                console.log('zaznacz odpowiedź')
             } else {
                 console.info('last question');
             }
         }
 
-        return {answersArray, questions, questionNo, quizStarted, startQuiz}
+        const checkAnswer = () => {
+            console.log('answer:', userAnswer.value);
+            if (userAnswer.value) {
+                userAnswers.push(userAnswer.value);
+            } else {
+                console.log('brak odp')
+            }
+            console.log('answers:', userAnswers);
+        }
+
+        const setAnswer = answer => {
+            userAnswer.value = answer;
+
+            console.log('set:', userAnswer.value)
+
+            return answer;
+        };
+
+        return {answersArray, questions, questionNo, userAnswer,  checkAnswer, quizStarted, startQuiz, setAnswer}
     }
 }
 </script>

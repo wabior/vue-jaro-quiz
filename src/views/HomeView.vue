478c0ba914c18b21c20508d79d9fcdf8efd6687e
jarek.wabich <jaroslaw.wabich@wfirma.pl>
2022-06-24 21:34:14 +0200

    answer options by v-for

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,18 @@
 <template xmlns="http://www.w3.org/1999/html">
-  <div class="home container bg-black py-5">
+    <div class="home container bg-black py-5">
 
-    <h1 class="mb-5">Wins Quiz</h1>
-    <button v-if="!quizStarted" @click="startQuiz" class="btn btn-light mb-5">Rozpocznij quiz</button>
-<!--       <h3 v-if="quizStarted">{{ questions[questionNo] }}</h3>-->
-    <div v-if="quizStarted">
-<!--    <div>-->
-       <h3 class="pb-3">{{ questions[questionNo]['question'] }}</h3>
-        <div class="row mx-auto justify-content-md-center pb-5 gx-5">
-          <div class="col-md-5 p-4"><button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo]['answer_a'] }}</button></div>
-          <div class="col-md-5 p-4"><button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo]['answer_b'] }}</button></div>
-          <div class="col-md-5 p-4"><button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo]['answer_c'] }}</button></div>
-          <div class="col-md-5 p-4"><button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo]['answer_d'] }}</button></div>
+        <h1 class="mb-5">Wins Quiz</h1>
+        <button v-if="!quizStarted" @click="startQuiz" class="btn btn-light mb-5">Rozpocznij quiz</button>
+        <div v-if="quizStarted">
+            <h3 class="pb-3">{{ questions[questionNo]['question'] }}</h3>
+            <div class="row mx-auto justify-content-md-center pb-5 gx-5">
+                <div v-for="answerOption in answersArray" class="col-md-5 p-4">
+                    <button class="btn btn-outline-dark py-3 w-100 text-light">{{ questions[questionNo][`answer_${answerOption}`] }}</button>
+                </div>
+            </div>
+            <button v-if="quizStarted" @click="startQuiz" class="btn btn-outline-dark mb-5">DALEJ</button>
         </div>
-      <button v-if="quizStarted" @click="startQuiz" class="btn btn-outline-dark mb-5">DALEJ</button>
     </div>
-  </div>
 
 </template>
 
@@ -23,33 +20,35 @@
 import {ref, watchEffect} from "vue";
 
 const API_URL = 'http://127.0.0.1:8000/api/question/';
+// const API_URL = 'http://quiz/api/question/';
 
 export default {
-  name: 'HomeView',
-  setup() {
-    const questions = ref([]),
-        questionNo = ref(0),
-        quizStarted = ref(false);
+    name: 'HomeView',
+    setup() {
+        const questions = ref([]),
+            questionNo = ref(0),
+            quizStarted = ref(false),
+            answersArray = ref(['a', 'b', 'c', 'd']);
 
-    watchEffect(async () => {
-      questions.value = await (await fetch(`${API_URL}`)).json();
-    })
+        watchEffect(async () => {
+            questions.value = await (await fetch(`${API_URL}`)).json();
+        })
 
-    const startQuiz = () => {
-      const questionsCount = questions.value.length;
+        const startQuiz = () => {
+            const questionsCount = questions.value.length;
 
-      if (!quizStarted.value) {
-        quizStarted.value = true
-      }
+            if (!quizStarted.value) {
+                quizStarted.value = true
+            }
 
-      if (questionNo.value < questionsCount - 1) {
-        questionNo.value++;
-      } else {
-        console.info('last question');
-      }
+            if (questionNo.value < questionsCount - 1) {
+                questionNo.value++;
+            } else {
+                console.info('last question');
+            }
+        }
+
+        return {answersArray, questions, questionNo, quizStarted, startQuiz}
     }
-
-    return { questions, questionNo, quizStarted, startQuiz }
-  }
 }
 </script>
